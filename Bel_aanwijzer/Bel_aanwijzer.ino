d758abd3b5aa7053814d85e57927dbdee793bd91
wvmarle <wouter@squirrel-systems.com>
2023-02-01 23:21:39 +0100

    Added Arduino sketch

---
+++ Bel_aanwijzer/Bel_aanwijzer.ino
@@ -0,0 +1,30 @@
+/*
+   "bel" pijl aanwijzer
+*/
+
+const uint8_t N_LAMPJES = 8;                                // De losse lampjes.
+const uint8_t PIN_LAMPJE[N_LAMPJES] = {4, 5, 6, 7, 8, 9, 10, 11};
+const uint8_t PIN_SENSOR = 12;
+const uint32_t SPEED = 400;                                 // Tijd per stap in ms.
+
+void setup() {
+  for (uint8_t i = 0; i < N_LAMPJES; i++) {
+    pinMode(PIN_LAMPJE[i], OUTPUT);
+    digitalWrite(PIN_LAMPJE[i], LOW);
+  }
+}
+
+void loop() {
+  if (digitalRead(PIN_SENSOR)) {                            // Sensor output is actief HIGH
+    for (uint8_t i = 0; i < N_LAMPJES; i++) {
+      digitalWrite(PIN_LAMPJE[i], HIGH);
+      delay(SPEED);
+    }
+    delay(SPEED);
+    delay(SPEED);
+    for (uint8_t i = 0; i < N_LAMPJES; i++) {
+      digitalWrite(PIN_LAMPJE[i], LOW);
+    }
+    delay(SPEED);
+  }
+}
